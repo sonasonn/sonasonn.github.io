6c9c23024937a4ed88ddfbb28b0ff74615cdde99
sonasonn <spetros13@students.lcc.lt>
2015-02-19 22:53:08 +0200

    New layout
    Added everything

---
+++ htm/style.js
@@ -0,0 +1,7 @@
+
+if (navigator.appVersion.indexOf('Mac')==-1) {
+document.write('<link href="style.css" rel="stylesheet" type="text/css">') 
+}
+else {
+document.write('<link href="style_mac.css" rel="stylesheet" type="text/css">') 
+}
